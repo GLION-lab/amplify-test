21b601fa48e2ea8a243a2cf2becbb7d233e839b3
addwis <addwis.jp@gmail.com>
2020-04-11 03:49:05 +0900

    Add: AuthとStorageを追加

--- src/App.vue
+++ src/App.vue
@@ -1,95 +1,142 @@
 <template>
   <div id="app">
-    <label for="check">
-      <h1>{{ is_appsync ? "Datastore(AppSync)テスト" : "GraphQLのテスト" }}</h1>
-    </label>
-    <input id="check" type="checkbox" v-model="is_appsync">
-    <!-- <button id="mugen" @click="mugen">無限</button> -->
-    <!-- AppSync -->
-    <template v-if="is_appsync">
-      <div>
-        <h3>Save Data</h3>
-        <label for="title">Title</label>
-        <input id="title" type="text" v-model="title">
-        <button @click="saveData">save</button>
-      </div>
-      <div>
-        <h3>Query Data
-          <span>合計: {{ data.length }}件</span>
-          <button @click="queryData">全件取得</button>
-          <button @click="data = []">クリア</button>
-        </h3>
+    <a @click.prevent="is_storage = !is_storage">{{ is_storage ? 'AppSync/GraphQL' : 'Storage/Auth' }}機能のテストはこちらをクリック</a>
+
+    <!-- Storage/Auth -->
+    <template v-if="is_storage">
+      <h1>Storage/Authのテスト</h1>
+      <p>テスト用にアカウントを作成、または下記情報でログインしてStorage機能をテストしてください</p>
+      <p>
+        email: test@mail.com
+        <br>password: password
+      </p>
+      <amplify-authenticator :authConfig="authConfig"></amplify-authenticator>
+      <template v-if="is_signedIn">
+        <amplify-sign-out></amplify-sign-out>
         <p>
-          <label for="search">Search</label>
-          <input id="search" type="text" v-model="filter">
-          <button @click="filterData">Title検索</button>
+          <label for="path">アップロード先のパス</label>
+          <input type="text" id="path" v-model="photoPickerConfig.path" placeholder="/">
         </p>
-        <table>
-          <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Rating</th>
-            <th>Status</th>
-            <th></th>
-          </tr>
-          <tr v-for="(item, index) in data" :key="index">
-            <td>{{ item.id }}</td>
-            <td>{{ item.title }}</td>
-            <td>{{ item.rating }}</td>
-            <td>{{ item.status }}</td>
-            <td>
-              <button @click="deleteData(item)">Delete</button>
-              <button @click="updateData(item)">Update</button>
-            </td>
-          </tr>
-        </table>
-      </div>
+        <amplify-photo-picker :photoPickerConfig="photoPickerConfig"></amplify-photo-picker>
+        <amplify-s3-album path="/"></amplify-s3-album>
+      </template>
     </template>
-    <!-- GraphQL -->
-    <template v-else-if="!is_appsync">
-      <div>
-        <h3>Save Data</h3>
-        <label for="title_ql">Title</label>
-        <input id="title_ql" type="text" v-model="title">
-        <button @click="saveGql">save</button>
-      </div>
-      <div>
-        <h3>Query Data
-          <span>合計: {{ data.length }}件</span>
-          <button @click="queryGql(null)">全件取得</button>
-          <button @click="queryGql(100)">100件取得</button>
-          <button @click="data = []">クリア</button>
-        </h3>
+
+    <!-- AppSync/GraphQL -->
+    <template v-else>
+      <label for="check">
+        <h1>{{ is_appsync ? "Datastore(AppSync)テスト" : "GraphQLのテスト" }}</h1>
+      </label>
+      <a @click.prevent="is_appsync = !is_appsync">{{ is_appsync ? 'GraphQL' : 'AppSync' }}機能のテストはこちらをクリック</a>
+      <br>
+      <a href="#s3">S3の画像</a>
+      <!-- <button id="mugen" @click="mugen">無限</button> -->
+
+      <!-- AppSync -->
+      <template v-if="is_appsync">
         <p>
-          <label for="search_ql">Search</label>
-          <input id="search_ql" type="text" v-model="filter">
-          <button @click="filterGql">Title検索</button>
+          DataStoreでは初期表示時にバックエンドでデータを同期します。
+          <br>初期同期の確認: chromeのシークレットモードでブラウザの開発者ツールNetworkタブを開いた状態でアクセスしてください。
+          <br>100件ずつに分けて全件取得している様子を確認できます。
         </p>
-        <table>
-          <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Rating</th>
-            <th>Status</th>
-            <th></th>
-          </tr>
-          <tr v-for="(item, index) in data" :key="index">
-            <td>{{ item.id }}</td>
-            <td>{{ item.title }}</td>
-            <td>{{ item.rating }}</td>
-            <td>{{ item.status }}</td>
-            <td>
-              <button @click="deleteGql(item)">Delete</button>
-              <button @click="updateGql(item)">Update</button>
-            </td>
-          </tr>
-        </table>
-      </div>
+        <div>
+          <h3>Save Data</h3>
+          <label for="title">Title</label>
+          <input id="title" type="text" v-model="title">
+          <button @click="saveData">save</button>
+        </div>
+        <div>
+          <h3>Query Data
+            <span>合計: {{ data.length }}件</span>
+            <button @click="queryData">全件取得</button>
+            <button @click="data = []">クリア</button>
+          </h3>
+          <p>
+            <label for="search">Search</label>
+            <input id="search" type="text" v-model="filter">
+            <button @click="filterData">Title検索</button>
+          </p>
+          <table>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Rating</th>
+              <th>Status</th>
+              <th></th>
+            </tr>
+            <tr v-for="(item, index) in data" :key="index">
+              <td>{{ item.id }}</td>
+              <td>{{ item.title }}</td>
+              <td>{{ item.rating }}</td>
+              <td>{{ item.status }}</td>
+              <td>
+                <button @click="deleteData(item)">Delete</button>
+                <button @click="updateData(item)">Update</button>
+              </td>
+            </tr>
+          </table>
+        </div>
+      </template>
+
+      <!-- GraphQL -->
+      <template v-else-if="!is_appsync">
+        <p>
+          Datastoreとは異なり、バックエンドでの同期はありません。
+          <br>1度でデータを取得している様子が確認できます。
+          <br>動作自体はRestAPIと同様になります。
+          <br>現在、Datastoreの同期と同居した状態になっているため、更新削除はエラーで動作しません。（保存取得の確認のみ可能）
+        </p>
+        <div>
+          <h3>Save Data</h3>
+          <label for="title_ql">Title</label>
+          <input id="title_ql" type="text" v-model="title">
+          <button @click="saveGql">save</button>
+        </div>
+        <div>
+          <h3>Query Data
+            <span>合計: {{ data.length }}件</span>
+            <button @click="queryGql(null)">全件取得</button>
+            <button @click="queryGql(100)">100件取得</button>
+            <button @click="data = []">クリア</button>
+          </h3>
+          <p>
+            <label for="search_ql">Search</label>
+            <input id="search_ql" type="text" v-model="filter">
+            <button @click="filterGql">Title検索</button>
+          </p>
+          <table>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Rating</th>
+              <th>Status</th>
+              <th></th>
+            </tr>
+            <tr v-for="(item, index) in data" :key="index">
+              <td>{{ item.id }}</td>
+              <td>{{ item.title }}</td>
+              <td>{{ item.rating }}</td>
+              <td>{{ item.status }}</td>
+              <td>
+                <button @click="deleteGql(item)">Delete</button>
+                <button @click="updateGql(item)">Update</button>
+              </td>
+            </tr>
+          </table>
+        </div>
+      </template>
+
+      <h3 id="s3">S3の画像</h3>
+      <amplify-s3-album path="/"></amplify-s3-album>
+
     </template>
+
   </div>
 </template>
 
 <script>
+// Auth
+import { AmplifyEventBus } from 'aws-amplify-vue';
 // AppSync
 import { DataStore } from "@aws-amplify/datastore"
 import { Post, PostStatus } from "./models"
@@ -104,6 +151,37 @@ import * as mutations from './graphql/mutations';
 export default {
   name: 'App',
   data: () => ({
+    // Auth
+    is_storage: false,
+    authConfig: {
+      usernameAttributes: 'Email',
+      signUpConfig: {
+        header: 'My Customized Sign Up',
+        hideAllDefaults: true,
+        defaultCountryCode: '1',
+        signUpFields: [
+          {
+            label: 'Email',
+            key: 'username',
+            required: true,
+            displayOrder: 1,
+            type: 'string',
+          },
+          {
+            label: 'Password',
+            key: 'password',
+            required: true,
+            displayOrder: 2,
+            type: 'password'
+          }
+        ]
+      }
+    },
+    // Storage
+    photoPickerConfig: {
+      path: '/'
+    },
+    // AppSync, GraphQL
     is_appsync: true,
     data: [],
     title: '',
@@ -114,9 +192,21 @@ export default {
     //   console.log(msg.model, msg.opType, msg.element)
     // })
   // },
-  // mounted() {
-  //   this.queryData()
-  // },
+  computed: {
+    is_signedIn() {
+      const flg = localStorage.getItem('amplify-signin-with-hostedUI')
+      return flg
+    }
+  },
+  mounted() {
+    // this.queryData()
+    AmplifyEventBus.$on('authState', res => {
+      console.log(`Auth event info: ${res}`)
+    });
+    AmplifyEventBus.$on('fileUpload', res => {
+      console.log(`File Upload info: ${res}`)
+    });
+  },
   methods: {
     async saveData() {
       await DataStore.save(
@@ -208,6 +298,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+a {
+  color: blue;
+  cursor: pointer;
 }
 
 table {
